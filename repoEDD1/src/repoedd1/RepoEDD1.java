26e571a5f0b7326db0af974b3e390e287fe268b5
REBE <REBE@REBE-PC>
2017-02-23 17:30:16 +0100

    Cambio 1 Netbenans.

--- repoEDD1/src/repoedd1/RepoEDD1.java
+++ repoEDD1/src/repoedd1/RepoEDD1.java
@@ -7,9 +7,7 @@ public class RepoEDD1 {
 
     public static void main(String[] args) {
         
-        System.out.print ("Hello World.");
-        System.out.println ("IDE Eclipse.");
-        System.out.println ("Segunda modificación.");
+        System.out.println ("IDE Netbeans.");
 
     }
     
