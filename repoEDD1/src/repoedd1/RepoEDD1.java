b514fddea4ebb1e024025db4d73e71ef8757e02e
REBE <REBE@REBE-PC>
2017-02-23 18:12:56 +0100

    Rama en Netbeans

--- repoEDD1/src/repoedd1/RepoEDD1.java
+++ repoEDD1/src/repoedd1/RepoEDD1.java
@@ -7,11 +7,11 @@ public class RepoEDD1 {
 
     public static void main(String[] args) {
         
-        System.out.print ("Hello World.");
+        System.out.println ("Hello World.");
         System.out.println ("IDE Eclipse.");
         System.out.println ("Segunda modificación.");
-        System.out.println ("IDE Netbeans");
-        System.out.pirntln ("Rama en Netbeans");
+        System.out.println ("IDE Netbeans.");
+        System.out.println ("Rama en Netbeans.");
         
 
     }
